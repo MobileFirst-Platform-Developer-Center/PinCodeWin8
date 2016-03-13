b685e2fc58af04d4f9e9208f2f57c5f21c5b386c
VIVIN KRISHNAN <vivinkrishnan@in.ibm.com>
2016-03-13 20:31:57 +0530

    PinCodeWin8 sample

---
+++ PincodeWin8/PincodeWin8.Shared/PinCodeChallengeHandler.cs
@@ -0,0 +1,117 @@
+﻿/**
+* Copyright 2016 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Worklight;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Net;
+
+namespace PincodeWin8
+{
+    public class PinCodeChallengeHandler : Worklight.ChallengeHandler
+    {
+        public JObject challengeAnswer { get; set; }
+        private bool authSuccess = false;
+        private bool shouldsubmitchallenge = false;
+        private bool shouldsubmitfailure = false;
+        private string Realm;
+
+        public static ManualResetEvent waitForPincode = new ManualResetEvent(false);
+
+        public PinCodeChallengeHandler(String securityCheck) 
+        {
+            Realm = securityCheck;
+        }
+
+        public override JObject GetChallengeAnswer()
+        {
+            return this.challengeAnswer;
+        }
+
+        public override string GetRealm()
+        {
+            return Realm;
+        }
+
+        public override void HandleChallenge(WorklightResponse challenge)
+        {
+
+            waitForPincode.Reset();
+            MainPage._this.showChallenge(challenge);
+            shouldsubmitchallenge = true;
+            waitForPincode.WaitOne();
+        }
+
+        public override bool ShouldSubmitFailure()
+        {
+            return shouldsubmitfailure;
+        }
+
+        public override void OnFailure(WorklightResponse response)
+        {
+            Debug.WriteLine(response.ResponseJSON);
+        }
+
+        public override void OnSuccess(WorklightResponse challenge)
+        {
+            Debug.WriteLine(challenge.ResponseJSON);
+        }
+
+        public override bool ShouldSubmitSuccess()
+        {
+            return authSuccess;
+        }
+
+        public override WorklightResponse GetSubmitFailureResponse()
+        {
+            JObject respJSON = new JObject();
+            respJSON.Add("Respose", "Cancelled Request");
+
+            WorklightResponse response = new WorklightResponse(false, "User cancelled the request", respJSON, "User cancelled the request", (int)HttpStatusCode.InternalServerError);
+            return response;
+        }
+
+        public override bool IsCustomResponse(WorklightResponse response)
+        {
+            if (response == null || response.ResponseJSON == null || response.ResponseJSON["PinCodeAttempts"] == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public override bool ShouldSubmitChallengeAnswer()
+        {
+            return this.shouldsubmitchallenge;
+        }
+
+
+        public void SetShouldSubmitChallenge(bool shouldsubmitchallenge)
+        {
+            this.shouldsubmitchallenge = shouldsubmitchallenge;
+        }
+
+        public void SetSubmitFailure(bool shouldsubmitfailure)
+        {
+            this.shouldsubmitfailure = shouldsubmitfailure;
+        }
+        
+    }
+}
---
+++ PincodeWin8/PincodeWin8.Windows/MainPage.xaml.cs
@@ -0,0 +1,178 @@
+﻿/**
+* Copyright 2016 IBM Corp.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Worklight;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace PincodeWin8
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static MainPage _this;
+        PinCodeChallengeHandler pinCodeChallengeHandler;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            _this = this;
+            pinCodeChallengeHandler = new PinCodeChallengeHandler("PinCodeAttempts");
+        }
+
+        private async void GetBalance_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                pinCodeChallengeHandler = (PinCodeChallengeHandler)getChallengeHandler();
+                pinCodeChallengeHandler.SetShouldSubmitChallenge(false);
+                pinCodeChallengeHandler.SetSubmitFailure(false);
+
+                IWorklightClient _newClient = WorklightClient.CreateInstance();
+                
+                _newClient.RegisterChallengeHandler(pinCodeChallengeHandler);
+
+                StringBuilder uriBuilder = new StringBuilder().Append("/adapters").Append("/ResourceAdapter").Append("/balance");
+
+                Debug.WriteLine(new Uri(uriBuilder.ToString(), UriKind.Relative));
+                
+                WorklightResourceRequest rr = _newClient.ResourceRequest(new Uri(uriBuilder.ToString(), UriKind.Relative), "GET", "accessRestricted");
+
+                WorklightResponse resp = await rr.Send();
+
+                System.Diagnostics.Debug.WriteLine(resp.ResponseText);
+
+                AddTextToConsole(resp.ResponseText);
+                
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.StackTrace);
+            }
+        }
+        
+        public void AddTextToConsole(String consoleText)
+        {
+            CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                 async () =>
+                 {
+                     MainPage._this.Console.Text = consoleText;
+
+                 });
+        }
+
+        private void ClearConsole(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            Console.Text = "";
+        }
+
+        private void ShowConsole(object sender, TappedRoutedEventArgs e)
+        {
+            MainPage._this.ConsolePanel.Visibility = Visibility.Visible;
+            MainPage._this.ConsoleTab.Foreground = new SolidColorBrush(Colors.DodgerBlue);
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            JObject pinJSON = new JObject();
+            pinJSON.Add("pin", pintext.Text);
+            _this.pintext.Text = "";
+            pinCodeChallengeHandler.challengeAnswer = pinJSON;
+            PinCodeChallengeHandler.waitForPincode.Set();
+            hideChallenge();
+        }
+
+        public async void showChallenge(WorklightResponse challenge)
+        {
+            String errorMsg = "";
+           
+            if (challenge.ResponseJSON["errorMsg"] != null )                
+            {
+                if (challenge.ResponseJSON["errorMsg"].Type == JTokenType.Null)
+                    errorMsg = "This data requires a PIN Code.\n";
+            }
+            
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                 async () =>
+                 {
+                     _this.HintText.Text = "";
+                     _this.LoginGrid.Visibility = Visibility.Visible;
+                     if(errorMsg != "")
+                     {
+                         _this.HintText.Text = errorMsg + "Remaining Attempts: " + challenge.ResponseJSON["remainingAttempts"];
+                     }
+                     else
+                     {
+                         _this.HintText.Text = challenge.ResponseJSON["errorMsg"] +"\n" + "Remaining Attempts: " + challenge.ResponseJSON["remainingAttempts"];
+                     }
+                     
+                     _this.GetBalance.IsEnabled = false;
+                 });
+        }
+
+        public void hideChallenge()
+        {
+            CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                async () =>
+                {
+                    MainPage._this.LoginGrid.Visibility = Visibility.Collapsed;
+                    MainPage._this.GetBalance.IsEnabled = true;
+                });
+            //pinCodeChallengeHandler.SetShouldSubmitChallenge(false);
+        }
+        
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            hideChallenge();
+            pinCodeChallengeHandler.SetSubmitFailure(true);
+            pinCodeChallengeHandler.SetShouldSubmitChallenge(false);
+            PinCodeChallengeHandler.waitForPincode.Set();
+        }
+
+        public ChallengeHandler getChallengeHandler()
+        {
+            return pinCodeChallengeHandler;
+        }
+
+        private void ShowInfo(object sender, TappedRoutedEventArgs e)
+        {
+            MainPage._this.ConsolePanel.Visibility = Visibility.Collapsed;
+            MainPage._this.ConsoleTab.Foreground = new SolidColorBrush(Colors.Gray);
+        }
+        
+    }
+}
